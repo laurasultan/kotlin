a775ede9627ae1579b469bb58a581ce93f0f4d3d
laura <105574779+laurasultan@users.noreply.github.com>
2023-08-22 10:10:50 -0300

    Teclado Main.kt

--- ProjKCalculosM/src/main/kotlin/Main.kt
+++ ProjKCalculosM/src/main/kotlin/Main.kt
@@ -1,43 +1,26 @@
 fun main(args: Array<String>) {
-    val x : Int
-    val y : Double
+    print("Informe um valor inteiro qualquer: ")
+    val valor1 : Int = readln().toInt()
 
-    x = 10
-    y = 15.5
+    print("Informe um valor númerico qualquer: ")
+    val valor2 : Float = readln().toFloat()
 
-    val z : Double = x + y
+    val soma : Float = valor1 + valor2
+    val subtracao : Float = valor1 - valor2
+    val multiplicacao : Float = valor1 * valor2
+    val divisao : Float = valor1 / valor2
+    val restoDaDivisao : Float = valor1 % valor2
 
-    print("A soma entre $x e $y é: $z")
+    println("\n\nA soma entre $valor1 e $valor2 é: $soma")
+    println("A subtração entre $valor1 e $valor2 é: $subtracao")
+    println("A multiplicação entre $valor1 e $valor2 é: $multiplicacao")
+    println("A divisão entre $valor1 e $valor2 é: $divisao")
+    println("O resto da divisão entre $valor1 e $valor2 é: $restoDaDivisao")
 }
 
 ----------------------------------------------------------------------------------
 
-fun main() {
-    print("Informe o valor de x:")
-    val x = readLine()?.toIntOrNull()
-
-    print("Informe o valor de y: ")
-    val y = readLine()?.toDoubleOrNull()
-
-//     if (x == null || y == null) {
-//         println("Informe valores válidos")
-//         return
-//     }
-
-    val soma = x + y
-    val sub = x - y
-    val mult = x * y
-    val div = x / y
-    val mod = x % y
-
-    println("A soma entre $x e $y é: $soma\n")
-    println("A subtração entre $x e $y é: $sub\n")
-    println("A multiplicação entre $x e $y é: $mult\n")
-    println("A divisão entre $x e $y é: $div\n")
-    println("A módulo de $x e $y é: $mod\n")
-}
-
-----------------------------------------------------------------------------------
+/*
 
 import java.util.Scanner
 
@@ -65,3 +48,5 @@ fun main() {
     println("A divisão entre $x e $y é: $div\n")
     println("A módulo de $x e $y é: $mod\n")
 }
+
+*/
