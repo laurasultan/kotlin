af13b82fe440755a4d57ea46259e0f0f3c9b37ea
laura <105574779+laurasultan@users.noreply.github.com>
2023-08-24 11:20:25 -0300

    Update Main.kt

--- ProjKSalarioBaseM/src/main/kotlin/Main.kt
+++ ProjKSalarioBaseM/src/main/kotlin/Main.kt
@@ -1,9 +1,14 @@
 fun main(args: Array<String>) {
     print("Informe o salário base: ")
-    val salarioBase : Double = readln().toDouble()
+    val salarioBase : Float = readln().toFloat()
 
-    val gratificacao : Double = salarioBase + salarioBase * 0.05
-    val imposto : Double = gratificacao - gratificacao * 0.07
+    // val gratificacao : Float = salarioBase * 0.05f
+    // val imposto : Float = gratificacao - gratificacao * 0.07f
 
-    println("\nSalário a receber: $imposto")
+    val salarioLiquido : Float = salarioBase * 1.05f * 0.93f
+
+    println("\nSalário a receber: $salarioLiquido")
 }
+
+// desconto = 0.
+// acrescimo = 1.
