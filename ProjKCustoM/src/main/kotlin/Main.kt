c523935c1113d97e2bd5c379e214f11701ed3d4b
laura <105574779+laurasultan@users.noreply.github.com>
2023-08-24 11:02:19 -0300

    Update Main.kt

--- ProjKCustoM/src/main/kotlin/Main.kt
+++ ProjKCustoM/src/main/kotlin/Main.kt
@@ -2,11 +2,13 @@ fun main(args: Array<String>) {
     print("Informe o custo de fábrica: ")
     val custoFabrica : Double = readln().toDouble()
 
-    val distribuidor : Double = 0.28
-    val impostos : Double = 0.45
+    // val distribuidor : Double = 0.28
+    // val impostos : Double = 0.45
+    // val custoConsumidor : Double = custoFabrica + custoFabrica * 0.73
+    
+    val custoConsumidor : Double = custoFabrica * 1.73
 
-    val custoConsumidor : Double = custoFabrica + (custoFabrica * 0.28) + (custoFabrica * 0.45)
-
-    println("\nCusto de fábrica: $custoFabrica")
-    println("Custo ao consumidor: $custoConsumidor")
+    println("\n\nDados de venda do veículo " +
+            "\nCusto de fábrica: $custoFabrica" +
+            "\nCusto ao consumidor: $custoConsumidor")
 }
