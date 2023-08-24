f67e7304da62ff4b2887e1447006883aa0ace867
laura <105574779+laurasultan@users.noreply.github.com>
2023-08-24 11:32:21 -0300

    Update Main.kt

--- ProjKProdutoM/src/main/kotlin/Main.kt
+++ ProjKProdutoM/src/main/kotlin/Main.kt
@@ -6,17 +6,17 @@ fun main(args: Array<String>) {
     val quantidade : Int = readln().toInt()
 
     print("Informe o valor unitário do produto: ")
-    val valorUnitario : Double = readln().toDouble()
+    val valorUnitario : Float = readln().toFloat()
 
-    val valorTotal : Double = quantidade * valorUnitario
-    val acrescimo : Double = valorTotal + valorTotal * 0.035
-    val desconto : Double = valorTotal - valorTotal * 0.025
+    val valorTotal : Float = quantidade * valorUnitario
+    val acrescimo : Float = valorTotal * 0.035f
+    val desconto : Float = valorTotal * 0.975f    // 0.025
 
-    println("\nDados do produto:")
-    println("Nome: $nome")
-    println("Quantidade: $quantidade")
-    println("Valor unitário: $valorUnitario")
-    println("Valor total: $valorTotal")
-    println("Valor total com 3,5% de acréscimo: $acrescimo")
-    println("Valor total com 2,5% de desconto: $desconto")
+    println("\n\nDados do produto\n " +
+            "Nome: $nome\n" +
+            "Quantidade: $quantidade\n" +
+            "Valor unitário: $valorUnitario\n" +
+            "Valor total: $valorTotal\n" +
+            "Valor total com 3,5% de acréscimo: $acrescimo\n" +
+            "Valor total com 2,5% de desconto: $desconto")
 }
