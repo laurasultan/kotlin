404d0c3918d24b802d0fe5abc33d16b4c4410b04
laura <105574779+laurasultan@users.noreply.github.com>
2023-08-24 09:48:04 -0300

    Update Main.kt

--- ProjKQuadradoM/src/main/kotlin/Main.kt
+++ ProjKQuadradoM/src/main/kotlin/Main.kt
@@ -2,8 +2,8 @@ fun main(args: Array<String>) {
     print("Informe um número inteiro: ")
     val inteiro : Int = readln().toInt()
 
-    val quadrado : Int = inteiro * inteiro
-    // val quadrado : Double = Math.pow(inteiro.toDouble())
+    // val quadrado : Int = inteiro * inteiro
+    val quadrado : Double = Math.pow(inteiro.toDouble(), 2.0)
 
-    println("\nO quadrado de $inteiro é = $quadrado")
+    println("\nO quadrado de $inteiro é $quadrado")
 }
